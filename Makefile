c8ad0896cdaf65277c9be7cfc9d47acdfedffe94
vicalvez <vicalvez@42nice.fr>
2023-11-29 13:51:55 +0100

    calloc

--- Makefile
+++ Makefile
@@ -6,15 +6,19 @@ OBJS= $(SRCS:.c=.o)
 BONUS_SRCS= ft_lstnew_bonus.c ft_lstadd_front_bonus.c ft_lstsize_bonus.c ft_lstlast_bonus.c ft_lstadd_back_bonus.c ft_lstdelone_bonus.c ft_lstclear_bonus.c ft_lstiter_bonus.c ft_lstmap_bonus.c
 BONUS_OBJS = $(BONUS_SRCS:.c=.o)
 
+ifdef BONUS
 ALL_OBJS= $(OBJS) $(BONUS_OBJS)
+else
+ALL_OBJS= $(OBJS)
+endif
 
 CC= gcc -Wall -Wextra -Werror
 
-$(NAME): $(OBJS)
-	ar -rcs $(NAME) $(OBJS)
-
-bonus: $(ALL_OBJS)
+$(NAME): $(ALL_OBJS)
 	ar -rcs $(NAME) $(ALL_OBJS)
+
+bonus:
+	$(MAKE) BONUS=1
 
 all: $(NAME)
 
--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: vicalvez <vicalvez@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 14:06:30 by vicalvez          #+#    #+#             */
-/*   Updated: 2023/11/29 10:40:50 by vicalvez         ###   ########.fr       */
+/*   Updated: 2023/11/29 13:43:34 by vicalvez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	void	*ptr;
 
 	if (size <= 0)
-		return (malloc(1));
+		return (malloc(0));
 	if (nmemb == 0 || size >= SIZE_MAX)
 		return (NULL);
 	ptr = malloc(nmemb * size);
